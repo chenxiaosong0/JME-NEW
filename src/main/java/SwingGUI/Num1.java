139de26747cef20b6d9ddaa3787e0a59352a494c
Chenxiaosong <1280639976@qq.com>
2022-11-15 21:52:36 +0800

    jme画布现在能正常显示在JFrame画布中。

--- src/main/java/SwingGUI/Num1.java
+++ src/main/java/SwingGUI/Num1.java
@@ -54,7 +54,8 @@ public class Num1 extends SimpleApplication {
         app.setShowSettings(false);
         AppSettings settings = new AppSettings(true);
         settings.setCustomRenderer(AwtPanelsContext.class);
-        settings.setFrameRate(60);
+        app.setPauseOnLostFocus(false);
+//        settings.setFrameRate(60);
         app.setSettings(settings);
         app.start();
 
@@ -62,11 +63,16 @@ public class Num1 extends SimpleApplication {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException exception) {
+                    return;
+                }
                 final AwtPanelsContext ctx = (AwtPanelsContext) app.getContext();
-                System.out.println(ctx);
                 panel = ctx.createPanel(PaintMode.Accelerated);
-                panel.setPreferredSize(new Dimension(400, 300));
+                panel.setPreferredSize(new Dimension(app.getCenter().getWidth(), app.getCenter().getHeight()));
                 ctx.setInputSource(panel);
+
                 createWindowForPanel(panel, 300);
                 panelsAreReady.countDown();
             }
@@ -95,14 +101,12 @@ public class Num1 extends SimpleApplication {
         menuBar.add(file);
         menuBar.add(New);
         frame.setMenuBar(menuBar);
-        Label label = new Label("This is  Center");
         button = new JButton("Click");
         center.setBackground(Color.lightGray);
         Top.setBackground(Color.white);
         tree1.setFont(new Font("JetBrains Mono", Font.BOLD, 14));
         bottom.setBackground(Color.LIGHT_GRAY);
         bottom.add(button);
-        center.add(label);
         app.action();
     }
 
@@ -112,13 +116,12 @@ public class Num1 extends SimpleApplication {
         frame.setContentPane(app.start);
         frame.setLocation(400, 200);
         frame.setResizable(true);
-        frame.setSize(1000, 600);
+//        frame.setSize(1000, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();//自适应
         app.createUIComponents(frame);
+        app.addAWTtoCenter(panel);//将jme的AWT画布添加进JPanel中
         frame.setVisible(true);
-
-        frame.add(panel, BorderLayout.CENTER);
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosed(WindowEvent e) {
@@ -128,6 +131,10 @@ public class Num1 extends SimpleApplication {
         });
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void addAWTtoCenter(AwtPanel panel) {
+        center.add(panel);
     }
 
     //jme的画面
@@ -147,7 +154,7 @@ public class Num1 extends SimpleApplication {
         } catch (InterruptedException exception) {
             throw new RuntimeException("Interrupted while waiting for panels", exception);
         }
-        panel.attachTo(true, viewPort);
+        panel.attachTo(false, viewPort);//true窗口出现加载混乱
         guiViewPort.setClearFlags(true, true, true);
     }
 
@@ -221,8 +228,8 @@ public class Num1 extends SimpleApplication {
         if (startFont != null) start.setFont(startFont);
         start.setForeground(new Color(-8221551));
         start.setInheritsPopupMenu(true);
-        start.setMinimumSize(new Dimension(1000, 800));
-        start.setPreferredSize(new Dimension(1000, 800));
+        start.setMinimumSize(new Dimension(1200, 800));
+        start.setPreferredSize(new Dimension(1200, 800));
         start.setToolTipText("3333");
         start.setVerifyInputWhenFocusTarget(false);
         start.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(), "", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.BELOW_TOP, this.$$$getFont$$$(null, -1, -1, start.getFont()), new Color(-4473925)));
@@ -235,16 +242,15 @@ public class Num1 extends SimpleApplication {
         Top.add(button1, BorderLayout.WEST);
         bottom = new JPanel();
         bottom.setLayout(new BorderLayout(0, 0));
-        bottom.setMinimumSize(new Dimension(150, 100));
+        bottom.setMinimumSize(new Dimension(1200, 100));
         bottom.setPreferredSize(new Dimension(100, 100));
         start.add(bottom, BorderLayout.SOUTH);
-        final JLabel label1 = new JLabel();
-        label1.setText("Meeeage.......");
-        bottom.add(label1, BorderLayout.CENTER);
         Left = new JPanel();
         Left.setLayout(new BorderLayout(0, 0));
         start.add(Left, BorderLayout.CENTER);
         splitPane = new JSplitPane();
+        splitPane.setDividerLocation(150);
+        splitPane.setDividerSize(3);
         Left.add(splitPane, BorderLayout.CENTER);
         splitPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
         center = new JPanel();
@@ -260,22 +266,22 @@ public class Num1 extends SimpleApplication {
         splitPane.setLeftComponent(tree1);
         Right = new JPanel();
         Right.setLayout(new BorderLayout(0, 0));
-        Right.setMinimumSize(new Dimension(200, 49));
-        Right.setPreferredSize(new Dimension(200, 49));
+        Right.setMinimumSize(new Dimension(160, 49));
+        Right.setPreferredSize(new Dimension(160, 49));
         start.add(Right, BorderLayout.EAST);
         num1 = new JScrollPane();
         num1.setPreferredSize(new Dimension(52, 20));
         Right.add(num1, BorderLayout.CENTER);
-        final JLabel label2 = new JLabel();
-        label2.setText("message");
-        num1.setViewportView(label2);
+        final JLabel label1 = new JLabel();
+        label1.setText("message");
+        num1.setViewportView(label1);
         num2 = new JScrollPane();
         num2.setMinimumSize(new Dimension(14, 200));
         num2.setPreferredSize(new Dimension(31, 300));
         Right.add(num2, BorderLayout.SOUTH);
-        final JLabel label3 = new JLabel();
-        label3.setText("Label");
-        num2.setViewportView(label3);
+        final JLabel label2 = new JLabel();
+        label2.setText("Label");
+        num2.setViewportView(label2);
         start.setNextFocusableComponent(start);
     }
 
